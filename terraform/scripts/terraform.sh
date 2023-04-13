f7051e9334fc75a644939967939d04368caf1003
jkritzen <j.kritzen@ontracon.de>
2023-04-13 23:42:15 +0200

    fix plan

--- terraform/scripts/terraform.sh
+++ terraform/scripts/terraform.sh
@@ -105,7 +105,6 @@ function plan(){
     echo -e "${OK}$TERRAFORM_ACTION${NC}: running terraform plan $TFVARS${NC}"
     terraform -chdir=$TF_DIR plan $TFVARS -input=false -out $TF_DIR/tf.plan
     terraform -chdir=$TF_DIR show -json $TF_DIR/tf.plan > $TF_DIR/tf.plan.json.local
-    terraform show -no-color plan.local > $TF_DIR/plan.out.local
     STDOUT=$(cat $TF_DIR/plan.out.local)
     # We need to strip the single quotes that are wrapping it so we can parse it with JQ
     plan=$(cat $TF_DIR/tf.plan.json.local | sed "s/^'//g" | sed "s/'$//g")
